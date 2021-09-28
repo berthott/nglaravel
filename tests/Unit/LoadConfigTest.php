907f8e0c75da2ad3974b16cbe4ff6d46da519d4c
Jan Bladt <jan.bladt@syspons.com>
2021-09-28 12:20:23 +0200

    fix test

--- tests/Unit/LoadConfigTest.php
+++ tests/Unit/LoadConfigTest.php
@@ -7,10 +7,9 @@ use berthott\NgLaravel\Tests\TestCase;
 class LoadConfigTest extends TestCase
 {
     /** @test */
-    function load_config()
+    public function load_config()
     {
         $this->assertSame('assets/angular', config('angular.output'));
-        $this->assertSame('angular', config('angular.route.prefix'));
-        $this->assertSame(['web'], config('angular.route.middleware'));
+        $this->assertSame(['web'], config('angular.middleware'));
     }
 }
