1bf0d37b7bf0ae788dca1d41729e16e4f6b02927
Jan Bladt <jan.bladt@syspons.com>
2021-09-28 12:22:53 +0200

    make any parameter optional

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::any('/{any}', [NgController::class, 'index'])
+Route::any('/{any?}', [NgController::class, 'index'])
   ->where('any', '^(?!api).*$')
   ->name('index');
